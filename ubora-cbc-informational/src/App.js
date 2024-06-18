de03d2e3dcf118cb85aed9997498fead644db965
esther-njoroge <nyokabinjorogeesther@gmail.com>
2024-06-18 15:28:00 +0300

    add faqs

--- ubora-cbc-informational/src/App.js
+++ ubora-cbc-informational/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Content from './Faqs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Questions = () =>{
+  return(
+    <div>
+      <h1 id='faqs'>FAQS</h1>
+      <Content/>
     </div>
-  );
-}
+  )
+};
 
-export default App;
+
+export default Questions;
---
+++ ubora-cbc-informational/src/Faqs/index.js
@@ -0,0 +1,52 @@
+import './index.css'
+// import questionmark from "../images/questionmark.webp";
+// import minus from "../images/minus.png";
+// import plus from "../images/plus.png"
+
+
+const  Content = () =>{
+    return(
+        <div className="intro">
+        <h2 className='heading'>Frequently Asked Questions</h2>
+        <p className='par'>Below are answers to some of the most frequently asked questions (FAQS) about Ubora and our platform.</p>
+
+
+        <div className='questions'>
+        <p className='que'>What is Ubora and what does it offer ?<img src="/images/minus.png" alt='minus' className='minus'/></p>
+        <p>Ubora is a platform build to improve CBC student engagement during online learning. It <br></br>offers different content organized by different topics. </p>
+        <hr className='line'></hr>
+        </div>
+        
+        <div className='questions'>
+        <p>What kind of educational content does Ubora offer? <img src="/images/plus.png" alt='plus' className='plus'/></p>
+        <hr className='line'></hr>
+        </div>
+        
+        <div className='questions'>
+        <p>Do I need internet to access content ? <img src="/images/plus.png" alt='plus' className='plus-sec'/></p>
+        <hr className='line'></hr>
+        </div>
+       
+       <div className='questions'>
+        <p>What subjects are currently available on the platform ? <img src="/images/plus.png" alt='plus' className='plus-third'/></p>
+        <hr className='line'></hr>
+        </div>
+
+       <div className='search'>
+       <img src="/images/questionmark.webp" alt='questionmark' className='search-image'/>
+         
+        <div className='text-over'>
+        <p className='search-par'><b>What can we help you find?</b></p>
+        <form>
+            <input type='search' placeholder='search...' className='search-button'></input>
+            <button className='search-submit'>search</button>
+        </form>
+        </div> 
+
+        </div>
+
+        </div>
+    )
+};
+
+export default Content;
