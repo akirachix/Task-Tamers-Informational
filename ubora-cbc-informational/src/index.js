8dd74d5ad7570307f7a0c9684591d35e537cf6ae
student <tuukuodorcas@gmail.com>
2024-07-25 08:32:52 +0300

    made changes

--- ubora-cbc-informational/src/index.js
+++ ubora-cbc-informational/src/index.js
@@ -27,15 +27,6 @@ root.render(
     <App />
   </Router>
 
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
 
 );
