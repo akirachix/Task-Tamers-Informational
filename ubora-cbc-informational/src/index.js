38bbd88cd3005926406e3980094b9f3465212df6
student <tuukuodorcas@gmail.com>
2024-07-25 17:58:21 +0300

    add files

--- ubora-cbc-informational/src/index.js
+++ ubora-cbc-informational/src/index.js
@@ -16,16 +16,18 @@
 
 import React from 'react';
 
-import ReactDOM from 'react-dom/client'; // Use createRoot for React 18
+import ReactDOM from 'react-dom/client'; 
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css'; // Global styles (optional)
+import './index.css'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<React.StrictMode>
   <Router>
     <App />
   </Router>
+  </React.StrictMode>
 
 
 
